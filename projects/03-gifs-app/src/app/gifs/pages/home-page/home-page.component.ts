777d192a159b750749b998d2b18d6b451671c92a
Roger davy Caichihua vergara <rcaichihua@gmail.com>
2023-12-15 13:39:50 -0500

    primera carga, creacion de componentes home, sidebar.

---
+++ projects/03-gifs-app/src/app/gifs/pages/home-page/home-page.component.ts
@@ -0,0 +1,7 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'gifs-home-page',
+  templateUrl: './home-page.component.html'
+})
+export class HomePageComponent { }
