e5034e0d93d52526215fc000eed227f2841337f1
Roger Davy Caichihua <rcaichihuav@gmail.com>
2023-12-15 18:29:23 -0500

    Se agrego card y search-box para home page

---
+++ projects/03-gifs-app/src/app/gifs/components/card-list/card-list.component.ts
@@ -0,0 +1,7 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'gifs-card-list',
+  templateUrl: './card-list.component.html'
+})
+export class CardListComponent { }
